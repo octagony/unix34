bf46299ce28628ed160658b3be86f3f36157c0c1
octagony <@moctagony@gmail.com>
2023-03-22 20:10:01 +0300

    [FEAT] Setup layout

--- src/components/layout/index.tsx
+++ src/components/layout/index.tsx
@@ -1,12 +1,21 @@
 import { FC, PropsWithChildren } from "react";
 import { IMeta } from "../seo/IMeta";
+import Meta from "../seo/Meta";
+import Header from "./Header";
+import Footer from "./Footer";
 
 const Layout: FC<PropsWithChildren<IMeta>> = ({
   title,
   description,
   children,
 }) => {
-  return <p>Layout</p>;
+  return (
+    <Meta title={title} description={description}>
+      <Header />
+      {children}
+      <Footer />
+    </Meta>
+  );
 };
 
 export default Layout;
--- src/components/screens/Home/index.tsx
+++ src/components/screens/Home/index.tsx
@@ -1,7 +1,12 @@
+import Layout from "@/components/layout";
 import { NextPage } from "next";
 
 const HomePage: NextPage = () => {
-  return <p>Home Page</p>;
+  return (
+    <Layout title="Home" description="Home page">
+      <h1>Welcome</h1>
+    </Layout>
+  );
 };
 
 export default HomePage;
